51ea74cb4f875566f64e47222bfa349d3ba0e6a4
Shreya-web20 <shreya.sgh05@gmail.com>
2026-09-07 14:28:44 +0530

    Fix authentication cookie for production

--- server/server.js
+++ server/server.js
@@ -116,8 +116,8 @@ app.post("/login", loginLimiter, (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 60 * 60 * 1000
     });
 
